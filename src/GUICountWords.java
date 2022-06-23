8d1cc1611781357ace4b5e592ddf2defe9ceadd0
G R <ger.roither@gmail.com>
2022-06-23 10:40:30 +0200

    commit

--- src/GUICountWords.java
+++ src/GUICountWords.java
@@ -40,7 +40,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
