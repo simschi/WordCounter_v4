49262a3a6994cba07c44ff23bbc3ca0676d287f0
G R <ger.roither@gmail.com>
2022-06-29 10:25:56 +0200

    commit

--- src/CountWords.java
+++ src/CountWords.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
